3f7fc745b450816e6c4673b60315fb1187515039
Yacob Madiana <yacob.madiana@gmail.com>
2020-11-08 16:22:02 +0700

    Styling login screen v1

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,5 @@
 import React, {FC, useState} from 'react';
 import {
-  Button,
   View,
   Text,
   TextInput,
@@ -19,35 +18,53 @@ const Login: FC<TypeProps> = (props) => {
   const [password, setPassword] = useState('');
 
   return (
-    <View style={styles.viewStyle}>
-      <Text>Nomer HP</Text>
-      <TextInput
-        style={styles.textInputStyle}
-        keyboardType="numeric"
-        onChangeText={(text) => setUsername(text)}
-        value={username}
-      />
-      <Text>Password</Text>
-      <TextInput
-        style={styles.textInputStyle}
-        secureTextEntry={true}
-        onChangeText={(text) => setPassword(text)}
-        value={password}
-      />
-      <TouchableOpacity
-        style={styles.loginButtonStyle}
-        onPress={() => props.navigation.navigate('Home')}>
-        <Text>Login</Text>
-      </TouchableOpacity>
+    <View style={styles.containerView}>
+      <View style={styles.viewStyle}>
+        <Text>Nomer HP</Text>
+        <TextInput
+          style={styles.textInputStyle}
+          keyboardType="numeric"
+          onChangeText={(text) => setUsername(text)}
+          value={username}
+        />
+        <Text>Password</Text>
+        <TextInput
+          style={styles.textInputStyle}
+          secureTextEntry={true}
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+        />
+        <TouchableOpacity
+          style={styles.loginButtonStyle}
+          onPress={() => props.navigation.navigate('Home')}>
+          <Text>LOGIN</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bottomViewStyle}>
+        <TouchableOpacity
+          style={styles.registerButtonStyle}
+          onPress={() => props.navigation.navigate('Home')}>
+          <Text>Belum Terdaftar?</Text>
+          <Text>Daftar Kaum Saleh</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  containerView: {
+    flex: 1,
+    height: '100%',
+  },
   viewStyle: {
     flex: 1,
     alignItems: 'flex-start',
     justifyContent: 'flex-start',
+    margin: 20,
+  },
+  bottomViewStyle: {
+    alignSelf: 'center',
     margin: 20,
   },
   textInputStyle: {
@@ -63,12 +80,19 @@ const styles = StyleSheet.create({
     marginTop: 4,
     height: 48,
     width: 200,
-    textAlign: 'center',
+    textAlign: 'left',
     justifyContent: 'center',
     fontSize: 16,
     alignItems: 'center',
-    backgroundColor: '#DDDDDD',
+    backgroundColor: '#3283a8',
     borderRadius: 15,
+  },
+  registerButtonStyle: {
+    width: 200,
+    textAlign: 'left',
+    justifyContent: 'center',
+    fontSize: 16,
+    alignItems: 'center',
   },
 });
 
