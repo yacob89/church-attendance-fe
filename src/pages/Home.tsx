d9bd2e784c2f2ba6a54a7e5c64af7a7eba9f0e9a
Yacob Madiana <yacob.madiana@gmail.com>
2020-11-08 16:49:01 +0700

    Add dropdown picker with search

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,89 +1,119 @@
-import React, {FC} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {FC, useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+import Icon from 'react-native-vector-icons/Feather';
 
 interface TypeProps {
   title: string;
 }
 
 const Home: FC<TypeProps> = (props) => {
+  const [activity, setActivity] = useState('usa');
+  const [name, setName] = useState('');
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+    <View style={styles.containerView}>
+      <View style={styles.viewStyle}>
+        <Text>Aktivitas</Text>
+        <DropDownPicker
+          items={[
+            {
+              label: 'USA',
+              value: 'usa',
+              icon: () => <Icon name="flag" size={18} color="#900" />,
+              hidden: true,
+            },
+            {
+              label: 'UK',
+              value: 'uk',
+              icon: () => <Icon name="flag" size={18} color="#900" />,
+            },
+            {
+              label: 'France',
+              value: 'france',
+              icon: () => <Icon name="flag" size={18} color="#900" />,
+            },
+          ]}
+          defaultValue={activity}
+          containerStyle={{height: 40}}
+          style={styles.dropdownStyle}
+          itemStyle={{
+            justifyContent: 'flex-start',
+          }}
+          dropDownStyle={{backgroundColor: '#fafafa'}}
+          onChangeItem={(item) => setActivity(item.value)}
+          searchable={true}
+          searchablePlaceholder="Search for an item"
+          searchablePlaceholderTextColor="gray"
+          searchableError={() => <Text>Not Found</Text>}
+        />
+        <Text>Nama Kaum Saleh</Text>
+        <TextInput
+          style={styles.textInputStyle}
+          onChangeText={(text) => setName(text)}
+          value={name}
+        />
+        <TouchableOpacity
+          style={styles.loginButtonStyle}
+          onPress={() => console.log('Hoihoi')}>
+          <Text>ABSEN</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  buttonContainer: {
-    padding: 10,
-  },
-  buttonLanguage: {
-    marginBottom: 4,
-    height: 48,
-    textAlign: 'center',
-    justifyContent: 'center',
-    fontSize: 16,
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    borderRadius: 15,
-  },
-  textLanguage: {},
-  container: {
+  containerView: {
     flex: 1,
-    alignSelf: 'stretch',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginHorizontal: 16,
-    padding: 8,
-    textAlign: 'center',
-  },
-  fixToText: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  flagStyle: {
-    width: '100%',
-    height: 64,
-  },
-  pickerContainer: {
-    textAlign: 'center',
-    width: '100%',
-  },
-  pickerContainerInner: {
-    textAlign: 'center',
-    width: '70%',
-  },
-  pickerStyle: {
-    textAlign: 'center',
-    width: '70%',
-    alignSelf: 'stretch',
-  },
-  scrollView: {
-    marginHorizontal: 2,
     height: '100%',
   },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: '#737373',
-    borderBottomWidth: StyleSheet.hairlineWidth,
+  viewStyle: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    margin: 20,
   },
-  text: {
+  bottomViewStyle: {
+    alignSelf: 'center',
+    margin: 20,
+  },
+  dropdownStyle: {
+    backgroundColor: '#fafafa',
+    width: '75%',
+  },
+  textInputStyle: {
+    height: 40,
+    width: '75%',
+    paddingLeft: 20,
+    marginBottom: 20,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 20,
+  },
+  loginButtonStyle: {
+    marginTop: 4,
+    height: 48,
+    width: 200,
+    textAlign: 'left',
+    justifyContent: 'center',
     fontSize: 16,
+    alignItems: 'center',
+    backgroundColor: '#3283a8',
+    borderRadius: 15,
   },
-  title: {
-    textAlign: 'center',
-    marginVertical: 8,
-    fontSize: 20,
-    marginTop: 8,
-    marginBottom: 8,
-  },
-  textStyle: {
-    textAlign: 'center',
-  },
-  wrapperCustom: {
-    borderRadius: 8,
-    padding: 6,
+  registerButtonStyle: {
+    width: 200,
+    textAlign: 'left',
+    justifyContent: 'center',
+    fontSize: 16,
+    alignItems: 'center',
   },
 });
 
